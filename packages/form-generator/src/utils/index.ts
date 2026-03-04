25af4b6bfe2083b782b4bd46f7b10dea81209754
Pavel Polyakov <p.polyakov@cyrm.ru>
2026-03-04 16:20:40 +0300

    Add index.ts to isFieldRequired util

--- packages/form-generator/src/utils/index.ts
+++ packages/form-generator/src/utils/index.ts
@@ -1,3 +1,3 @@
 export { evaluateComputedValue } from './evaluateComputedValue';
 export { collectFieldsFromCondition, collectValidationMessages, evaluateConditions } from './evaluateConditions';
-export { isFieldRequired } from './isFieldRequired/isFieldRequired';
+export { isFieldRequired } from './isFieldRequired';
